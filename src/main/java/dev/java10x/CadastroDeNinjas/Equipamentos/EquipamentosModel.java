547a79c754fae7e504bffd08e592e72d78942ab9
chatfly <oiamigopao@outlook.com>
2025-02-06 17:39:12 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Equipamentos/EquipamentosModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Equipamentos/EquipamentosModel.java
@@ -2,9 +2,15 @@ package dev.java10x.CadastroDeNinjas.Equipamentos;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_equipamentos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EquipamentosModel {
 
     @Id
@@ -19,37 +25,5 @@ public class EquipamentosModel {
     @ManyToOne
     @JoinColumn (name = "ninja_id") // Foreign Key ou chave estrangeira
     private NinjaModel ninja;
-
-    public EquipamentosModel() {
-    }
-
-    public EquipamentosModel(String nome, String descricao) {
-        this.nome = nome;
-        this.descricao = descricao;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Equipamentos.EquipamentosModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -10,6 +13,9 @@ import java.util.List;
 // JPA = Java Persistance API
 @Entity // Identifica uma Entidade de banco de dados
 @Table(name = "tb_cadastro") // Identifica a tabela de banco de dados e seu nome
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id // Identifica que o atributo logo abaixo é um ID
@@ -27,36 +33,5 @@ public class NinjaModel {
     @OneToMany (mappedBy = "equipamentos")
     private List<EquipamentosModel> equipamentos;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
