120a287e425044752d3899a4d47f40643e18f4fa
chatfly <oiamigopao@outlook.com>
2025-02-08 16:32:06 -0300

    chore: .env adiciona as minhas credenciais no BD

--- src/main/java/dev/java10x/CadastroDeNinjas/Equipamentos/EquipamentosRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Equipamentos/EquipamentosRepository.java
@@ -2,5 +2,5 @@ package dev.java10x.CadastroDeNinjas.Equipamentos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EquipamentosRepository extends JpaRepository<EquipamentosModel, Long> {
+public interface EquipamentosRepository extends JpaRepository<EquipamentosModel, Long>  {
 }
