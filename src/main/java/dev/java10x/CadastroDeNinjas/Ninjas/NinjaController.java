1e86f84bb5b42db613fad1fbc255e1fc656e8b86
chatfly <oiamigopao@outlook.com>
2025-02-09 10:38:11 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -11,4 +11,34 @@ public class NinjaController {
         return "Essa é a minha primeira mensagem nessa rota";
     }
 
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninja";
+    }
+
+    // Mostrar ninja por id (READ)
+    @GetMapping("/todosID")
+    public String mostrarNinjaPorId() {
+        return "Mostrar Ninja por id";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja por id";
+    }
+
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarrNinjaPorId() {
+        return "Ninja deletado por id";
+    }
+
 }
