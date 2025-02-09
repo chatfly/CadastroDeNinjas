69783e1f05b509bd31782a82426c77861b464d6f
chatfly <oiamigopao@outlook.com>
2025-02-09 12:15:50 -0300

    Inclui a annotation @JsonIgnore no EquipamentosModel para previnir erros de serialização

--- src/main/java/dev/java10x/CadastroDeNinjas/Equipamentos/EquipamentosModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Equipamentos/EquipamentosModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Equipamentos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -24,6 +25,7 @@ public class EquipamentosModel {
     // Um equipamento pode ter apenas um ninja
     @ManyToOne
     @JoinColumn (name = "ninja_id") // Foreign Key ou chave estrangeira
+    @JsonIgnore
     private NinjaModel ninja;
 
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Equipamentos.EquipamentosModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
