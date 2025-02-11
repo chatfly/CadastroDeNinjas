1beface1b161f82c0933edd903f14a093aa24845
chatfly <oiamigopao@outlook.com>
2025-02-11 15:58:01 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos Entidade x DTO

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,23 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Equipamentos.EquipamentosModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private List<EquipamentosModel> equipamentos;
+
 }
