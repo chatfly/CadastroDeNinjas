285da3cb3410054083e8e0455c3678af026b7859
chatfly <oiamigopao@outlook.com>
2025-02-13 18:06:46 -0300

    chore: Remove os comentários TODO da classe Ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -7,11 +7,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin // TODO: ANOTAR SOBRE CROSSORIGIN
+@CrossOrigin
 @RequestMapping("/ninjas")
 public class NinjaController {
 
-    //TODO: ANOTAR SOBRE FINAL EM INJECOES DE DEPENDENCIA
     private final NinjaService ninjaService;
 
     public NinjaController(NinjaService ninjaService) {
@@ -21,30 +20,27 @@ public class NinjaController {
     @PostMapping("/criar")
     public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninja) {
         NinjaDTO novoNinja = ninjaService.criarNinja(ninja);
-        return ResponseEntity.status(HttpStatus.CREATED) // TODO: ANOTAR SOBRE OS TIPOS DE RESPONSE ENTITY E O BODY
+        return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Ninja criado com sucesso: " + novoNinja.getNome() + " (ID): " + novoNinja.getId());
     }
 
     @GetMapping("/listar")
     public ResponseEntity<List<NinjaDTO>> listarNinjas() {
-        return ResponseEntity.ok(ninjaService.listarNinjas()); // TODO: ANOTAR SOBRE O METODO OK DO RESPONSE ENTITY
+        return ResponseEntity.ok(ninjaService.listarNinjas());
     }
 
     @GetMapping("/listar/{id}")
     public ResponseEntity<?> listarNinjasPorId(@PathVariable Long id) {
-
         NinjaDTO ninja = ninjaService.listarNinjasPorId(id);
-
         if(ninja != null) {
             return ResponseEntity.ok(ninja);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body("Ninja com o ID: " + id + " não foi encontrado.");
-
     }
 
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<?> alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) { // TODO: ANOTAR SOBRE OS GENERICS EM RESPONSE ENTITY
+    public ResponseEntity<?> alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) {
 
         NinjaDTO ninja = ninjaService.atualizarNinja(id, ninjaAtualizado);
 
